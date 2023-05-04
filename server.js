6555a25008165d70b28bb007530a70c7e1207b54
Shashils <pargaonkarshashi@gmail.com>
2023-05-04 22:02:29 +0530

    First Commit that

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+let initial_path = path.join(__dirname, "public");
+
+const app = express();
+
+app.use(express.static(initial_path));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(initial_path, "home.html"));
+});
+
+app.get("/editor", (req, res) => {
+  res.sendFile(path.join(initial_path, "editor.html"));
+});
+
+app.listen("3000", () => {
+  console.log("listening........");
+});
